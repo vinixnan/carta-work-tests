330c6038429648a0105c50dafaec30e2c4716bbc
Vinicius Renan de Carvalho <vrcarvalho@usp.br>
2021-04-04 20:19:43 +0000

    Fix Role

--- init_db.sh
+++ init_db.sh
@@ -13,7 +13,7 @@ psql postgres -c "CREATE USER carta PASSWORD 'password'"
 #The -O flag below sets the user: createdb -O DBUSER DBNAME
 createdb -O carta carta
 
-psql postgres -c "GRANT ALL PRIVILEGES ON DATABASE carta TO carta"
+psql postgres -c "ALTER ROLE CARTA SUPERUSER"
 
 wget -O fhirbase-linux-amd64 https://github.com/fhirbase/fhirbase/releases/download/v0.0.6/fhirbase-linux-amd64?_ga=2.205532759.556026196.1617411701-717493598.1617411701
 chmod +x fhirbase-linux-amd64
